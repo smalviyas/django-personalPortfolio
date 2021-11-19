b82c1cc7839fe12d8f4570bd01cc84fa6b7cad16
smalviyas <shubham7879@gmail.com>
2021-11-19 14:28:49 +0530

    first commit

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.shortcuts import render
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateField()
+
+    def __str(self):
+        return self.title
+
